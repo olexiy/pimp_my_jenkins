dea891c7bbb2302d9fecd544beb214ec78d83d87
Olexiy Sokurenko <ol.sakura@gmail.com>
2018-05-04 09:34:28 +0200

    Dropping the BO and creating image based on the latest Jenkins Dockerfile

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ systemctl start docker
 usermod -aG docker ubuntu
 
 # run jenkins
-mkdir -p /var/jenkins_bo_home
-chown -R 1000:1000 /var/jenkins_bo_home/
-docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_bo_home:/var/jenkins_home -d --name blueocean_jenkins jenkinsci/blueocean
+mkdir -p /var/jenkins_home
+chown -R 1000:1000 /var/jenkins_home/
+docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins olexiy/jenkins-docker
 
 # show endpoint
 echo 'Jenkins installed'
