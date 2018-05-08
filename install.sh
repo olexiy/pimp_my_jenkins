5946e35e188269e59545e811d6c65e0ff8883107
Olexiy Sokurenko <ol.sakura@gmail.com>
2018-05-08 13:03:54 +0200

    Change docker repo to the artful - bionic still have no stable docker version

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ apt-get remove docker docker-engine docker.io
 apt-get update
 apt-get install  apt-transport-https  ca-certificates  curl  software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository   "deb [arch=amd64] https://download.docker.com/linux/ubuntu   $(lsb_release -cs)  stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu artful stable"
 apt-get update
-apt-get install docker-ce
+apt-get install docker-ce -y
 
 # run jenkins
 mkdir -p /var/jenkins_home
